c61b2ca7a1f219c96266927f206ab19e9250cd12
jungsangwon <top1tiger@naver.com>
2022-05-14 23:40:33 +0900

    Time: 19 ms (8.30%), Space: 42.6 MB (5.68%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        long result = 0;
+        long dvd = Math.abs((long)dividend);
+        long dvs = Math.abs((long)divisor);
+        boolean plus = true;
+        if(dividend<0){
+            plus = plus == false;
+        }
+        if(divisor<0){
+            plus = plus == false;
+        }
+        long check = dvs;
+        long count = 1;
+        if(dvd<dvs){
+            return 0;
+        }
+        System.out.println(dvd + " " +dvs);
+        while(dvd>=dvs){
+            while(dvd<check){
+                check -= dvs;
+                count--;
+            }
+            dvd -= check;
+            result += count;
+            check += dvs;
+            count ++;
+        }
+        if(!plus){
+            result = -result;
+        }
+        System.out.println(result);
+        if(result<=Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+        }else if(result>=Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }
+        return (int)result;
+    }
+}

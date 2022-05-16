72fe0c029eb808afe4d8473d43e1a1f46468298d
jungsangwon <top1tiger@naver.com>
2022-05-16 10:11:51 +0900

    Time: 76 ms (39.82%), Space: 44.4 MB (43.46%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -9,6 +9,8 @@ class Solution {
                     dp[i] = dp[j] + 1;
                 }
             }
+        }
+        for(int i=0;i<nums.length;i++){
             if(max<dp[i]){
                 max = dp[i];
             }

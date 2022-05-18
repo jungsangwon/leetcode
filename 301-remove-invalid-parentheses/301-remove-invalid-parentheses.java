4278d9de1020f0d43b1b052f4f68e45b3ba8d323
jungsangwon <top1tiger@naver.com>
2022-05-18 13:50:14 +0900

    Time: 101 ms (56.62%), Space: 74 MB (14.17%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        HashSet<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        q.add(s);
+        
+        List<String> ans = new ArrayList<>();
+        while (!q.isEmpty()) {
+            
+            String curr = q.poll();
+            if (this.valid(curr)) {
+                ans.add(curr);
+            }
+            
+            for (int i = 0; i < curr.length(); i++) {
+                
+                char ichar = curr.charAt(i);
+                if (ichar != '(' && ichar != ')') {
+                    continue;
+                }
+                String part1 = curr.substring(0, i);
+                String part2 = curr.substring(i + 1);
+                String next = part1 + part2;
+                if (!visited.contains(next)) {
+                    if (ans.isEmpty() || ans.get(ans.size() - 1).length() < next.length()) {
+                        visited.add(next);
+                        q.add(next);
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+   public boolean valid(String str) {
+        int n = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ichar = str.charAt(i);
+            if (ichar == '(') {
+                n++;
+            }
+            else if (ichar == ')') {
+                if (n == 0) {
+                    return false;
+                }
+                n--;
+            }
+        }
+        
+        return n == 0;
+    }
+}

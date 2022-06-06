289a53db2040bd146b25aeb03c8f42dac628c75d
jungsangwon <top1tiger@naver.com>
2022-06-07 00:38:05 +0900

    Time: 3 ms (56.95%), Space: 93.7 MB (43.29%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
@@ -1,40 +1,16 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        boolean[] dp = new boolean[nums.length];
-        boolean[] visit = new boolean[nums.length];
-        int m = Integer.MAX_VALUE;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]<m){
-                m=nums[i];
-            }else{
-                continue;
-            }
-            int min = Integer.MAX_VALUE;
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]<nums[j] && min>nums[j]){
-                    min = nums[j];
-                    if(isIncreasing(nums,j,dp,visit)){
-                        return true;
-                    }
+        int m1 = Integer.MIN_VALUE;
+        int m2 = Integer.MIN_VALUE;
+        for(int i =nums.length-1;i>=0;i--){
+            m1 = Math.max(m1,nums[i]);
+            if(m1>nums[i]){
+                m2 = Math.max(m2,nums[i]);
+                if(m2>nums[i]){
+                    return true;
                 }
             }
         }
         return false;
-    }
-    
-    public boolean isIncreasing(int[] nums, int idx, boolean[] dp, boolean[] visit){
-        if(visit[idx]){
-            return dp[idx];
-        }else{
-            visit[idx] = true;
-            for(int i=idx+1;i<nums.length; i++){
-                if(nums[idx]<nums[i]){
-                    dp[idx] = true;
-                    return true;
-                }
-            }
-            dp[idx] = false;
-            return false;
-        }
     }
 }

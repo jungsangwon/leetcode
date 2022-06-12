536088e3ddbca85c07e0fd8da539b21360477385
jungsangwon <top1tiger@naver.com>
2022-06-12 21:20:22 +0900

    Time: 115 ms (52.94%), Space: 76.5 MB (74.26%) - LeetHub

--- 327-count-of-range-sum/327-count-of-range-sum.java
+++ 327-count-of-range-sum/327-count-of-range-sum.java
@@ -1,79 +1,57 @@
 class Solution {
-    int count = 0;
-    int lb;
-    int ub;
-    long[] temp;
-    // be aware 
-    long[] nums; // use long since we are recording sum and nums migh be at limit of int
-    
-    public int countRangeSum(int[] ns, int lower, int upper) {
-        // dp or merge"sum" of elements between lower and upper, O(nlogn) time complexity
-        // in merge"sum" the left and right half was already counted, then we pick one from left and one from right and 
-        // work on presum, then the subarray sum equals preSum(i) - preSum(j)
-        // each merge subproblem is equal to finding n1[i] - n2[j] within range in 2 sorted arrays
-        lb = lower;
-        ub = upper;
-        temp = new long[ns.length+1];
-        nums = new long[ns.length+1];
-        
-        nums[0] = 0;
-        for(int i = 0; i < ns.length; i++) {
-            nums[i+1] = nums[i] + ns[i];
+    public int countRangeSum(int[] nums, int lower, int upper) {
+        long[] numList = new long[nums.length+1];
+        long[] tmpList = new long[nums.length+1];
+        numList[0] = 0;
+        for(int i=1;i<=nums.length;i++){
+            numList[i] = numList[i-1] + nums[i-1];
         }
-        sort(0, nums.length-1);
-        return count;
+        int[] result = new int[1];
+        merge(numList,tmpList,lower,upper,0,numList.length-1,result);
+        return result[0];
     }
     
-    // merge sort implementation
-    private void sort(int l, int h) {
-        if(l == h) return;
-        
-        int mid = l+(h-l)/2;
-        
-        // with recursion, the index range cases not on each halves have already been checked
-        sort(l,mid);
-        sort(mid+1,h);
-        merge(l,h,mid);
-    }
-    
-    // merge with counting items within range
-    private void merge(int l, int h, int mid) {
-        // copy sorted value to temp
-        for(int i = l; i <= h; i++) {
-            temp[i] = nums[i];
+    public void merge(long[] numList,long[] tmpList,int lower,int upper,int left, int right, int[] result){
+        if(left==right){
+            return;
         }
-        
-        // counting range sums index
-        // checking anything in range on 2 sorted arrays
-        int x=l;
-        int yl = mid+1;
-        int yh = mid+1;
-        while(x < mid+1) {
-            long xNum = temp[x];
-            while(yl <= h && temp[yl] - xNum < lb) {
-                yl++;
+        int mid = (right + left) / 2;
+        merge(numList,tmpList,lower,upper,left,mid,result);
+        merge(numList,tmpList,lower,upper,mid+1,right,result);
+        int l = left;
+        int tl = mid+1;
+        int tu = mid+1;
+        while(l<=mid){
+            while(tl<=right && numList[tl]-numList[l]<lower){
+                tl++;
             }
-            while(yh <=h && temp[yh] - xNum <= ub) {
-                yh++;
+            while(tu<=right && numList[tu]-numList[l]<=upper){
+                tu++;
             }
-            count+=yh-yl;
-            x++;
+            result[0] += (tu-tl);
+            l++;
         }
-        
-        // regular merge sort 
-        int i = l;
-        int j = mid+1;
-        
-        for(int p = l; p <= h; p++) {
-            if(i == mid+1) {
-                nums[p]=temp[j++];
-            } else if(j == h+1) {
-                nums[p]=temp[i++];
-            } else if(temp[i] <= temp[j]) {
-                nums[p] = temp[i++];
-            } else {
-                nums[p] = temp[j++];
+        int lidx = left;
+        int ridx = mid+1;
+        int idx = left;
+        while(lidx<=mid && ridx <=right){
+            if(numList[lidx]<numList[ridx]){
+                tmpList[idx++] = numList[lidx++];
+            }else{
+                tmpList[idx++] = numList[ridx++];
             }
+        }
+        if(lidx<=mid){
+            while(lidx<=mid){
+                tmpList[idx++] = numList[lidx++];
+            }
+        }else{
+            while(ridx<=right){
+                tmpList[idx++] = numList[ridx++];
+            }
+        }
+        for(int i=left; i<=right; i++){
+            numList[left++] = tmpList[i];
         }
     }
 }

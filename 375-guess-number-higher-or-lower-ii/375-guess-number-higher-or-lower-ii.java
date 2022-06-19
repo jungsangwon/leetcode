31311808c356066ac4d2117c00b20d19d9a911f8
jungsangwon <top1tiger@naver.com>
2022-06-19 20:30:21 +0900

    Time: 97 ms (12.96%), Space: 41.8 MB (72.63%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.java
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.java
@@ -3,18 +3,10 @@ class Solution {
         int result = Integer.MAX_VALUE;
         int[][] dp = new int[n+1][n+1];
         getMoneyAmount(1,n,dp);
-        // System.out.println("====");
-        // for(int i=1; i<=n; i++){
-        //     for(int j=i; j<=n; j++){
-        //         System.out.println(i+" -> "+j+" = "+dp[i][j]);
-        //     }
-        // }
-        
         return dp[1][n];
     }
     
     public int getMoneyAmount(int left, int right, int[][] dp){
-        // System.out.println(left+" ~ "+right);
         if(left==right){
             return 0;
         }else if(dp[left][right]!=0){

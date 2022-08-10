cfa599495aa8f97cf7b78df795fb87d576644a74
jungsangwon <top1tiger@naver.com>
2022-08-10 19:07:08 +0900

    Time: 15 ms (5.71%), Space: 43.5 MB (81.83%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        Arrays.fill(result,-1);
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i]==nums2[j]){
+                    for(int k=j+1;k<nums2.length;k++){
+                        if(nums2[j]<nums2[k]){
+                            result[i] = nums2[k];
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}

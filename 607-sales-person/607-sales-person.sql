112ffb15702cc9da999b84f3b34666f4bbcf7b82
jungsangwon <top1tiger@naver.com>
2022-09-15 22:49:33 +0900

    Time: 2104 ms (24.31%), Space: 0B (100.00%) - LeetHub

---
+++ 607-sales-person/607-sales-person.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select name
+from SalesPerson
+where sales_id not in (
+    select sales_id
+    from Orders
+    where com_id = (select com_id from Company where name ='RED' )
+)

fe5140535f4587acce9670cf96b86e3414ea7148
jungsangwon <top1tiger@naver.com>
2023-02-27 23:23:26 +0900

    Time: 5 ms (85.42%), Space: 43.8 MB (54.63%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,75 +1,30 @@
 class Solution {
-    int result = Integer.MAX_VALUE;
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        HashMap<Integer,Node> map = new HashMap<>();
-        HashMap<Node,TreeMap<Integer,Integer>> visit = new HashMap<>();
-        for(int i=0; i<n; i++){
-            map.put(i,new Node(i));
+        int[][] graph = new int[n][n];
+        for (int[] flight : flights) {
+            graph[flight[0]][flight[1]] = flight[2];
         }
-        for(int i=0; i<flights.length; i++){
-            Node from = map.get(flights[i][0]);
-            Node to = map.get(flights[i][1]);
-            from.addFlight(to,flights[i][2]);
-        }
-        TreeMap<Integer,Integer> tmap = new TreeMap<>();
-        tmap.put(k,0);
-        visit.put(map.get(src),tmap);
-        findCheapestPrice(map.get(src),map.get(dst),k,0, visit);
-        if(result == Integer.MAX_VALUE){
-            result = -1;
-        }
-        return result;
-    }
-    
-    public void findCheapestPrice(Node from, Node target, int k, int now, HashMap<Node,TreeMap<Integer,Integer>> visit){
-        if(now<result){
-            if(from == target){
-                result = Math.min(result, now);
-            }
-            if(k>=0){
-                for(Node next : from.flight.keySet()){
-                    int nextNum = now+from.flight.get(next);
-                    if(visit.containsKey(next)){
-                        boolean check = true;
-                        TreeMap<Integer,Integer> tmap = visit.get(next);
-                        for(int last : tmap.keySet()){
-                            if(last<k){
-                                continue;
-                            }else if(last==k){
-                                if(tmap.get(last)<=nextNum){
-                                    check = false;
-                                    break;
-                                }
-                            }else{
-                                if(tmap.get(last)<=nextNum){
-                                    check = false;
-                                }
-                                break;
-                            }
-                        }
-                        if(!check){
-                            continue;
-                        }
+
+        int[] distances = new int[n];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        distances[src] = 0;
+
+        int minCost = Integer.MAX_VALUE;
+
+        for (int stop = 0; stop <= k; stop++) {
+            int[] tempDistances = Arrays.copyOf(distances, n);
+            for (int[] flight : flights) {
+                int from = flight[0], to = flight[1], cost = flight[2];
+                if (distances[from] != Integer.MAX_VALUE && distances[from] + cost < tempDistances[to]) {
+                    tempDistances[to] = distances[from] + cost;
+                    if (to == dst) {
+                        minCost = Math.min(minCost, tempDistances[to]);
                     }
-                    TreeMap<Integer,Integer> tmap = visit.getOrDefault(next,new TreeMap<>());
-                    tmap.put(k,nextNum);
-                    visit.put(next,tmap);
-                    findCheapestPrice(next,target,k-1,nextNum,visit);
                 }
             }
+            distances = tempDistances;
         }
-    }
-}
 
-class Node{
-    int now;
-    HashMap<Node,Integer> flight;
-    public Node(int now){
-        this.now = now;
-        flight = new HashMap<>();
-    }
-    
-    public void addFlight(Node a, int num){
-        flight.put(a,num);
+        return minCost == Integer.MAX_VALUE ? -1 : minCost;
     }
 }

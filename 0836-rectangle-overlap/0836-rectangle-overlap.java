c879d4386ac75015c758e18ce1e3db7717fd91cd
jungsangwon <top1tiger@naver.com>
2023-04-05 23:48:56 +0900

    Time: 0 ms (100.00%), Space: 40 MB (37.94%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        if(rec1[0]>=rec2[2]||rec2[0]>=rec1[2]){
+            return false;
+        }
+        if(rec1[1]>=rec2[3]||rec2[1]>=rec1[3]){
+            return false;
+        }
+        return true;
+    }
+}
